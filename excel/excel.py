70a0230d899c3a8b334d13b760516d6c6870dd1a
CcharlotteHh <210547@student.glu.nl>
2022-10-10 17:12:05 +0200

    first commit

---
+++ excel/excel.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_excel (r'C:\xampp\htdocs\p5\python week 1\data-to-fish\excel\productlist.xlsx') #vind de excel file en zet dit in df 
+print (df) #print df zodat deze in de terminal word weergegeven
---
+++ screenshot/screenshot.py
@@ -0,0 +1,4 @@
+import pyautogui #importeert pyautogui
+
+myScreenshot = pyautogui.screenshot() #maakt screenshot
+myScreenshot.save(r'C:\xampp\htdocs\p5\python week 1\data-to-fish\screenshot\screenshot_1.jpg')#slaat screenshot op als op aangegeven locatie
